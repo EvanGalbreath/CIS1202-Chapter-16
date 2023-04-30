b5dc6f7f84bd72ad89d3930c052dd6f4fb048fc5
EVAN GALBREATH <evan.galbreath@sinclair.edu>
2023-04-30 15:55:37 -0400

    I created the main function and decided to just go with the example test statements.

---
+++ CIS1202 Chapter 16/Source.cpp
@@ -0,0 +1,23 @@
+//Evan Galbreath
+//CIS 1202
+// April 30th, 2023
+
+#include <iostream>
+
+using namespace std;
+
+char character(char, int);
+
+int main() {
+    cout << character('A', 1) << endl;
+    cout << character('a', -32) << endl;
+    cout << character('Z', -1) << endl;
+    cout << character('?', 5) << endl;
+
+    return 0;
+}
+
+char character(char start, int offset)
+{
+
+}
