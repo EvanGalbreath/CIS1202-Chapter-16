6fc81e3f7afaa472d46a8d74268df39647153bb6
EVAN GALBREATH <evan.galbreath@sinclair.edu>
2023-04-30 16:38:47 -0400

    I completed the character function, and the only way I could figure out to get the throw/catch in the function to work, was to create a class for the character exception and range exception. The classes are empty, and simply there to distinguish what type of error was encountered so the output can be correct.

---
+++ CIS1202 Chapter 16/Exception.h
@@ -0,0 +1,14 @@
+#ifndef EXCEPTION_H
+#define EXCEPTION_H
+
+class invalidCharacterException
+{
+	
+};
+
+class invalidRangeException
+{
+	
+};
+
+#endif
--- CIS1202 Chapter 16/Source.cpp
+++ CIS1202 Chapter 16/Source.cpp
@@ -3,12 +3,13 @@
 // April 30th, 2023
 
 #include <iostream>
-
+#include "Exception.h"
 using namespace std;
 
 char character(char, int);
 
-int main() {
+int main()
+{
     cout << character('A', 1) << endl;
     cout << character('a', -32) << endl;
     cout << character('Z', -1) << endl;
@@ -19,5 +20,40 @@ int main() {
 
 char character(char start, int offset)
 {
+    try
+    {
+        if (!isalpha(start)) //if the input char is not a character, it will give a character exception
+        {
+            throw invalidCharacterException();
+        }
 
+        char result = start + offset; //creating a variable for what the char will be after the change is made
+
+        if (!isalpha(result)) // if the result is not an alphanum char, but the input was, the range is a problem
+        {
+            throw invalidRangeException();
+        }
+
+        if (isupper(start) && islower(result)) // if the start and result are different capitilization, the ASCII value changed out of the range
+        {
+            throw invalidRangeException();
+        }
+
+        if (islower(start) && isupper(result)) //same as last but other way around
+        {
+            throw invalidRangeException();
+        }
+
+        return result; //if it got this far without running into an error, it will just display the result
+    }
+    catch (invalidCharacterException& e) //output if the exceptions are caught
+    {
+        cout << "Invalid character" << endl;
+    }
+    catch (invalidRangeException& e)
+    {
+        cout << "Invalid range" << endl;
+    }
+
+    return 0;
 }
